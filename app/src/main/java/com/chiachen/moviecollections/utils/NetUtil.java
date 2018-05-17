032f6c6019b230d32ac462d1a7f4aaa0153d6f4a
JasonChien <s124202468@gmail.com>
2018-05-17 10:36:48 +0800

    Refine network checker.

--- app/src/main/java/com/chiachen/moviecollections/utils/NetUtil.java
+++ app/src/main/java/com/chiachen/moviecollections/utils/NetUtil.java
@@ -2,15 +2,17 @@ package com.chiachen.moviecollections.utils;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
 public class NetUtil {
 
     public static boolean isNetworkAvailable(Context context) {
-        try{
+        try {
             ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-            return cm.getActiveNetworkInfo() != null;
-        }catch (Exception e){
-            return true;
+            NetworkInfo activityNetwork = cm.getActiveNetworkInfo();
+            return activityNetwork != null && activityNetwork.isConnectedOrConnecting();
+        } catch (Exception e) {
+            return false;
         }
     }
 }
